81e0b41d28d1246c46698dd1e2de59c5af3596d7
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 22:00:23 +0400

    Changed the program 001 lesson2

--- Lesson2/homework001/Program.cs
+++ Lesson2/homework001/Program.cs
@@ -5,8 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Введите целое 3х значное число ");
-            string number = Console.ReadLine().ToString();
-            Console.WriteLine($" 2я цивра в вашем числе {number[1]} ");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            if (numbers > 99 & numbers<1000)
+            {
+                string number = numbers.ToString();
+                Console.WriteLine($" 2я цивра в вашем числе {number[1]} "); 
+            }
+            else
+            {
+                Console.WriteLine($"Ваше число не 3х значное {numbers} ");
+            }
+           Console.ReadKey();
         }
     }
 }
