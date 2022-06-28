2529c881faa1ca7338aa6a559835c55288aa64cf
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:51:00 +0400

    add homework003 Lesson2

---
+++ Lesson2/homework003/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace homework003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер дня недели ");
+            int Day = Convert.ToInt32(Console.ReadLine());
+            switch (Day)    
+            {
+                case 1:
+                Console.WriteLine("Будний день: Говорят это самый тяжелый день Понедельник!!!");
+                break;
+                case 2:
+                Console.WriteLine("Будний день: Работать иди сегодня Вторкник!!");
+                break;
+                case 3:
+                Console.WriteLine("Будний день: Вот и половина сегодня все же на работу Среда!!!");
+                break;
+                case 4:
+                Console.WriteLine("Будний день: Потерпи осталось пару дней Четверг!!!");
+                break;
+                case 5:
+                Console.WriteLine("Будний день: Вот ты и на финишной прямой Пятница!!!");
+                break;
+                case 6:
+                Console.WriteLine("Выходной: УРАААА вот ты и дожил Суббота!!!");
+                break;
+                case 7:
+                Console.WriteLine("Выходной: Сегодня тоже можешь отдохнуть но не забывай завтра Понедельник Ну а сегодня Воскреснье!!!");
+                break;
+
+                default:
+                Console.WriteLine("УПС такого дня нет ");
+                break;
+            }
+           Console.ReadKey();
+        }
+    }
+}
