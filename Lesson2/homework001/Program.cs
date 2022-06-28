eb571cb7defd816bddc205eadc1c774db3146e9c
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:48:46 +0400

    add homework001 Lesson2

---
+++ Lesson2/homework001/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace homework001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое 3х значное число ");
+            string number = Console.ReadLine().ToString();
+            Console.WriteLine($" 2я цивра в вашем числе {number[1]} ");
+        }
+    }
+}
