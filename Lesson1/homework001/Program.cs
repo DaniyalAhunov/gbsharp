0938e36a907854449805471e92e4a84f3f0873c6
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:40:51 +0400

    add homework001 Lesson1

---
+++ Lesson1/homework001/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace example001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2ое целое число ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            if (number1 > number2)
+            {
+                Console.WriteLine($"Число {number1} больше {number2} ");
+            }
+            if(number1 == number2)  
+            {
+                Console.WriteLine($"Число {number1} равно числу {number2} ");
+            }
+            else    
+            {
+               Console.WriteLine($"Число {number2} больше {number1} "); 
+            }
+            Console.Read();
+           
+        }
+       
+    }
+}
