c47066806d862ec51470c6e508320f6a47a82aa0
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:45:31 +0400

    add dophomework005 Lesson1

---
+++ Lesson1/dophomework005/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace dophomework005
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количесво цифр:");
+            int i = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine($"Введите целое {i} значное число");
+            string number = Console.ReadLine();
+            if (int.TryParse(number, out int numbers))
+                {
+                    int end = int.Parse(number[i -1].ToString());
+                    Console.Write("Вы ввели число {0}", numbers);
+                    Console.Write($" Последняя цифра {end}");
+
+                }
+            else
+                {
+                    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+                }
+            
+           
+            Console.ReadKey();
+        }
+    }
+
+}
