a823506658df0acc3e7d75f1822e67a6438efa16
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:44:16 +0400

    add homework004 Lesson1

---
+++ Lesson1/homework004/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace example004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i <= number; i++)
+            {
+                if(i % 2 == 0)
+                {
+                   Console.Write($"{i}");
+                }
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
