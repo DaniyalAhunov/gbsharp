bf882bef0d01bc8664caee2b572c4f9d1a3a3f8d
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:43:22 +0400

    add homework003 Lesson1

---
+++ Lesson1/homework003/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace example003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"Число {number} четное ");
+            }
+            else    
+            {
+                Console.WriteLine($"Число {number} нечетное ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
