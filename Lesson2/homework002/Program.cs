dc6cce3f4a88c09fd010632a2574ade0e6e01c09
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:50:02 +0400

    add homework002 Lesson2

---
+++ Lesson2/homework002/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace homework002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число ");
+            int numbers = Convert.ToInt32(Console.ReadLine());
+            string number = numbers.ToString();
+            if(numbers > 99)
+            {
+                Console.WriteLine($" 3я цивра в вашем числе {number[2]} ");
+            }
+            else
+            {
+                Console.WriteLine("В вашем числе меньше 3х цифр"); 
+            }
+           Console.ReadKey();
+        }
+    }
+}
