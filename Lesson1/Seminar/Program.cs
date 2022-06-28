5509745c896aed71fa54c0a076580a2a4c0d6cd3
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:38:08 +0400

    add info seminar

---
+++ Lesson1/Seminar/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите 2 целое число: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int dubl = number2*number2;
+            if (number1 == dubl)
+            {
+                Console.WriteLine($"Число {number1} является квадратом {number2}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {number1} не является квадратом {number2}");
+            }
+        }
+    }
+}
+// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// 2. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
