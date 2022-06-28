f38dd4962577c9594b29d6bbfebf9a678de080b6
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-06-28 21:47:16 +0400

    add info seminar2

---
+++ Lesson2/Seminar/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace seminar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите целое число ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int ost = number1 % number2 ;
+            if (number1 > number2)
+            {
+                if (ost == 0)
+                {
+                    Console.WriteLine("Кратно");
+                }
+                if  (ost != 0)
+                {
+                    Console.WriteLine($"Не кратно! Остаток: {ost}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Первое число должно быть больше второго");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
