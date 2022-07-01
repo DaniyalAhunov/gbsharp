187e2a974b3b0f01fae5a279eed599300f1ad945
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-07-01 18:50:32 +0300

    add homework003Lesson3

---
+++ Lesson3/homework002/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace example001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты А точки через запятую ");
+            string a = Console.ReadLine().ToString();
+            string[] asplit = a.Split(',');
+            int xa = Convert.ToInt32(asplit[0]);
+            int ya = Convert.ToInt32(asplit[1]);
+            int za = Convert.ToInt32(asplit[2]);
+            Console.WriteLine("Введите координаты B точки через запятую ");
+            string b = Console.ReadLine().ToString();
+            string[] bsplit = b.Split(',');
+            int xb = Convert.ToInt32(bsplit[0]);
+            int yb = Convert.ToInt32(bsplit[1]);
+            int zb = Convert.ToInt32(bsplit[2]);
+            double ab = Ploskost(xa, ya, za, xb, yb, zb);
+            Console.WriteLine($"Точка в 3D простанстве {ab}");
+            Console.ReadKey();
+
+        }
+        static double Ploskost(int xa, int ya, int za, int xb, int zb, int yb)
+        {
+            double log = (xa + xb) * (xa + xb) + (ya + yb) * (ya + yb) + (za + zb) * (za + zb);
+            double ab = Math.Sqrt(log);
+            return ab;
+
+        }
+    }
+}
---
+++ Lesson3/homework003/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace example001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                PrintCub(i);
+            }
+        }
+        public static void PrintCub(int n)
+        {
+            Console.WriteLine(n * n * n);
+        }
+    }
+}
