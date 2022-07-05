a959a084151db98dea92bebfaf1aafd3b6ba90bf
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-07-05 10:50:53 +0300

    add HW1

--- Lesson4/homework001/Program.cs
+++ Lesson4/homework001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+namespace seminar4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число(Степень)");
+            int step = Convert.ToInt32(Console.ReadLine());
+            int result = Stepen(number, step);
+            Console.WriteLine($"{result}");
+
+        }
+        static int Stepen(int i, int j)
+        {
+            
+            int result = i;
+            for (int count = 1; count < j ; count++)
+            {
+                result = result*i;
+            }
+            return result;
+
+        }
+    }
+}
