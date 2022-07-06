443d0e1065bb4e136f85b7b3d95fcc39f8d17da6
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-07-06 18:46:20 +0300

    add HW3

--- Lesson4/homework003/Program.cs
+++ Lesson4/homework003/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт случайный массив случайного размера (от 5 до 10) элементов 
+//(значение элементов от 1 до 40) и выводит на экран массив квадратов этих чисел.
+namespace homework002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = new Random().Next(5, 11);
+            int[] arrayNumber = new int[count];
+            RandomAndPrint(arrayNumber);
+            Console.WriteLine();
+            Console.WriteLine("Массив Квадратов: ");
+            PrintArrayDouble(arrayNumber);
+            Console.ReadKey();
+        }
+        public static void RandomAndPrint(int[] array)
+        {
+            int count = array.Length;
+            for (int i = 0; i < count;i++)    
+            {
+                int number = new Random().Next(0, 41);
+                array[i] = number;
+                Console.Write($"{array[i]} ");
+            }
+        }  
+        public static void PrintArrayDouble(int[] array)
+        {
+
+            int count = array.Length;
+            for (int i = 0; i < count;i++)    
+            {
+                Console.Write($"{array[i] * array[i]} ");
+            }
+        } 
+        
+    }
+}
