efc7c5bab5e36d1ca1da95ea68925c30ea568136
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-07-10 12:53:50 +0300

    add HW2 Lesson5

--- Lesson5/homework002/Program.cs
+++ Lesson5/homework002/Program.cs
@@ -7,6 +7,50 @@ namespace homework002
     {
         static void Main(string[] args)
         {
+            int count = new Random().Next(5,11);
+            int[] arrayNumber = new int[count];
+            Console.WriteLine("Первоночальный массив: ");
+            FillArray(arrayNumber);
+            Console.WriteLine();
+            int sum = SumNechet(arrayNumber);
+            Console.WriteLine($"Сумма чисел нечётных элементов: {sum}");
+            Console.ReadKey();
+
         }
+        /// <summary>///
+        ///Заполнение массива///
+        /// </summary>///
+        public static void FillArray(int[] array)
+        #region 
+        {
+            
+            for (int i = 0; i < array.Length;i++)
+            {
+                int number = new Random().Next(-40,41);
+                array[i] = number;
+                Console.Write($"{array[i]} ");
+            }
+            
+
+        }
+        #endregion
+        /// <summary>///
+        ///Поиск нечётных элементов и их сумирование///
+        /// </summary>///
+        static int SumNechet(int[] array)
+        #region 
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = sum + Convert.ToInt32(array[i]);
+                    Console.WriteLine(sum);  
+                }
+            }
+            return sum;
+        }
+        #endregion
     }
 }
--- Lesson5/homework003/Program.cs
+++ Lesson5/homework003/Program.cs
@@ -6,6 +6,30 @@ namespace homework003
     {
         static void Main(string[] args)
         {
+            int count = new Random().Next(5,11);
+            int[] arrayNumber = new int[count];
+            Console.WriteLine("Первоночальный массив: ");
+            FillArray(arrayNumber);
+            Console.WriteLine();
+            int sum = SumNechet(arrayNumber);
+            Console.WriteLine($"Разнича между максимальным и минимальным числом: {sum}");
+            Console.ReadKey();
+
         }
+        /// <summary>///
+        ///Заполнение массива///
+        /// </summary>///
+        public static void FillArray(int[] array)
+        #region 
+        {
+            for (int i = 0; i < array.Length;i++)
+            {
+                int number = new Random().Next(0,1000);
+                array[i] = number;
+                Console.Write($"{array[i]} ");
+            }
+        }
+        #endregion
+        //Поиск максимального числа в массиве
     }
 }
