c632ff78ab0bdca4bcc225083d74de8272f25e06
DaniyalAhunov <daniyal.ahunov@icloud.com>
2022-07-11 23:18:35 +0300

    add seminar6

---
+++ Lesson6/Homework001/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace homework001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lesson6/Seminar6/Program.cs
@@ -0,0 +1,45 @@
+﻿//1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+//[1 2 3 4 5] -> [5 4 3 2 1]
+//[6 7 3 6] -> [6 3 7 6]
+//2. Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+namespace seminar6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int count = new Random().Next(5,11);
+          int[] array = new int[count]; 
+          FillArray(array);
+          PrintArray(array);
+          ReverseArray(array);
+          PrintArray(array);
+        }
+        public static void FillArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Random().Next(0, 99);
+            }
+
+        }
+        public static void ReverseArray(int[] array)
+        {
+            for (int i = 0; i < array.Length/2;i++)
+            {
+                int x = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = x;
+            }
+        }
+        public static void PrintArray(int[] array)
+        {
+            Console.Write("[ ");
+            for (int i = 0; i<array.Length;i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
